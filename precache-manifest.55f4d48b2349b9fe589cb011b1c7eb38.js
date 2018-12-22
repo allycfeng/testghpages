60e861a325b478d035ad49826f69038fd1b39be8
allycfeng <76328ching@gmail.com>
2018-12-23 06:26:03 +0800

    Updates

---
+++ precache-manifest.55f4d48b2349b9fe589cb011b1c7eb38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "15f3e21f0b19caef5c64",
+    "url": "/testghpages/static/js/runtime~main.15f3e21f.js"
+  },
+  {
+    "revision": "a946f4b538e545f53f57",
+    "url": "/testghpages/static/js/main.a946f4b5.chunk.js"
+  },
+  {
+    "revision": "794557b9fe620e78fc3d",
+    "url": "/testghpages/static/js/1.794557b9.chunk.js"
+  },
+  {
+    "revision": "aeac602923f19030e8ffaa7ba2b7ef92",
+    "url": "/testghpages/index.html"
+  }
+];
---
+++ static/js/main.a946f4b5.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,t){e.exports=t(4)},function(e,n,t){"use strict";t.r(n);var r=t(0),o=t.n(r),c=t(2);t.n(c).a.render(o.a.createElement(function(){return o.a.createElement("div",null,"Hi there!")},null),document.querySelector("#root"))}],[[3,2,1]]]);
+//# sourceMappingURL=main.a946f4b5.chunk.js.map
